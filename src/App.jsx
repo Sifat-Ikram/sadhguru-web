ab7139d9226b33f5086418e59011962da26d6296
Sifat-Ikram <sifatikram@gmail.com>
2024-12-27 20:34:31 +0600

    sadhguru website updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { IoCallSharp } from "react-icons/io5";
-import guru from "./assets/sidhguru.webp";
+import guru from "./assets/Black_and_White_Illustrative_Smart_Brain_Technology_Logo_1.png";
 import { BlandWebClient } from "bland-client-js-sdk";
 import generateSessionToken from "./utils/generateSessionToken";
 
@@ -49,13 +49,13 @@ const App = () => {
           className="h-52 w-52 rounded-full overflow-hidden"
         />
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-medium uppercase text-[#196469] text-center">
-            Sadhguru
+          <h1 className="text-base sm:text-xl md:text-3xl font-medium uppercase text-[#196469] text-center">
+            Mental Counselling AI
           </h1>
-          <p className="text-base font-semibold text-center text-[#196469]">
-            Need spiritual advice or guidance?
+          <p className="text-xs sm:text-sm md:text-base font-semibold text-center text-[#196469]">
+            Need mental advice or guidance?
           </p>
-          <p className="text-base font-semibold text-center text-[#196469]">
+          <p className="text-xs sm:text-sm md:text-base font-semibold text-center text-[#196469]">
             I'm here to help you 24/7
           </p>
         </div>
@@ -65,11 +65,11 @@ const App = () => {
       {callState === "idle" && (
         <button
           onClick={handleCall}
-          className="w-[15rem] sm:w-[18rem] lg:w-[22rem] rounded-[14px] bg-[#196469] hover:bg-[#165c5e] flex justify-center items-center gap-2 md:gap-3 lg:gap-4 p-3 transition-all duration-300 ease-in-out"
+          className="w-[13rem] sm:w-[15rem] md:w-[17rem] lg:w-[19rem] rounded-[14px] bg-[#196469] hover:bg-[#165c5e] flex justify-center items-center gap-2 md:gap-3 lg:gap-4 p-3 transition-all duration-300 ease-in-out"
         >
           <FaPhoneVolume className="text-white md:text-xl" />
-          <span className="text-white text-base sm:text-lg lg:text-2xl font-medium uppercase">
-            Talk to Sadhguru
+          <span className="text-white text-sm sm:text-base md:text-lg lg:text-xl font-medium uppercase">
+            Call for support
           </span>
         </button>
       )}
@@ -89,7 +89,7 @@ const App = () => {
       {/* Footer Section */}
       <div>
         <h1 className="text-base font-semibold text-center text-[#196469]">
-          Try free consulting with Sadhguru now!
+          Try free consulting now!
         </h1>
       </div>
       <div className="flex flex-col justify-center items-center space-y-5">
